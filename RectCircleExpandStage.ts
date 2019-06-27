17145f14c7cc54d77906c44b4fa1f102ef9bd413
Anwesh43 <anweshthecool0@gmail.com>
2019-06-27 09:13:31 +0530

    adding constants

---
+++ RectCircleExpandStage.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const nodes : number = 5
+const lines : number = 2
+const foreColor : string = "green"
+const backColor : string = "#BDBDBD"
